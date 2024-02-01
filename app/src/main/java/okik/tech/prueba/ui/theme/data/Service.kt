5ec5376dcb0278aa54463958460e497dbc8aebf2
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-31 23:58:11 -0600

    refactor: Change return type that supports supsend functions

--- app/src/main/java/okik/tech/prueba/ui/theme/data/Service.kt
+++ app/src/main/java/okik/tech/prueba/ui/theme/data/Service.kt
@@ -1,9 +1,9 @@
 package okik.tech.prueba.ui.theme.data
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface Service {
     @GET("categories")
-    suspend fun categoriesList(): Call<List<Category>>
+    suspend fun categoriesList(): Response<List<String>>
 }
