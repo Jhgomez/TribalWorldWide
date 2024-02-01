ea4500cc98fdbbc4317bd42416a2885dd87f3a85
Juan Gomez <juan.e.g.f@outlook.com>
2024-02-01 00:02:34 -0600

    chore: get categories on init

--- app/src/main/java/okik/tech/prueba/ChallengeViewModel.kt
+++ app/src/main/java/okik/tech/prueba/ChallengeViewModel.kt
@@ -2,25 +2,32 @@ package okik.tech.prueba
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okik.tech.prueba.ui.theme.domain.Category
 import okik.tech.prueba.ui.theme.domain.ChallengeRepository
 import javax.inject.Inject
 
+@HiltViewModel
 class ChallengeViewModel @Inject constructor(private val challengeRepository: ChallengeRepository): ViewModel() {
 
     private val _categories = MutableStateFlow<List<Category>>(emptyList())
     val categories = _categories.asStateFlow()
 
     init {
-
+        getCategories()
     }
 
-    fun getCategories() {
-        viewModelScope.launch {
-
+    private fun getCategories() {
+        viewModelScope.launch() {
+            val categories = challengeRepository.getCategories()
+            if (!categories.isNullOrEmpty()) {
+                _categories.value = categories
+            }
         }
     }
 }
