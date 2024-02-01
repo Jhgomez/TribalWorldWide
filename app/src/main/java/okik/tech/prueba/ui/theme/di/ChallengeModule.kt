79f8394149b3547cac99e4e0aaf87e9c9d97fa7d
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-31 23:55:42 -0600

    chore: Add moshi converter to service

--- app/src/main/java/okik/tech/prueba/ui/theme/di/ChallengeModule.kt
+++ app/src/main/java/okik/tech/prueba/ui/theme/di/ChallengeModule.kt
@@ -1,13 +1,17 @@
 package okik.tech.prueba.ui.theme.di
 
+import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import okik.tech.prueba.ui.theme.data.ChallengeRepositoryImpl
 import okik.tech.prueba.ui.theme.data.Service
 import okik.tech.prueba.ui.theme.domain.ChallengeRepository
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -15,8 +19,15 @@ import javax.inject.Singleton
 class ChallengeModule {
     @Provides @Singleton
     fun providesService(): Service {
+
+        val moshi = Moshi.Builder()
+            .build()
+
         return Retrofit.Builder()
-            .baseUrl("https://api.chucknorris.io/jokes")
+            .baseUrl("https://api.chucknorris.io/jokes/")
+            .addConverterFactory(MoshiConverterFactory.create(
+                moshi
+            ))
             .build()
             .create(Service::class.java)
     }
