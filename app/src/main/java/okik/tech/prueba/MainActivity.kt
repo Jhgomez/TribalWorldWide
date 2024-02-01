09d504d8ed996f2f0e810fb4569b7a70bab440e3
Juan Gomez <juan.e.g.f@outlook.com>
2024-02-01 09:14:04 -0600

    chore: collect flow safely and display card for each category

--- app/src/main/java/okik/tech/prueba/MainActivity.kt
+++ app/src/main/java/okik/tech/prueba/MainActivity.kt
@@ -3,32 +3,64 @@ package okik.tech.prueba
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.gestures.ScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 import okik.tech.prueba.ui.theme.PruebaTheme
+import okik.tech.prueba.ui.theme.domain.Category
 import okik.tech.prueba.ui.theme.domain.ChallengeRepository
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
+    private val challengeViewModel: ChallengeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PruebaTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize().verticalScroll(ScrollState(0))
+                        ) {
+                            val categories = challengeViewModel.categories.collectAsStateWithLifecycle().value
+                            if (categories.isNotEmpty()) {
+                                categories.forEach { category ->
+                                    Card(modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(48.dp)) {
+                                        Text(
+                                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                                            text = category.name,
+                                            fontSize = 24.sp
+                                        )
+                                    }
+                                }
+                            }
+                        }
                 }
             }
         }
